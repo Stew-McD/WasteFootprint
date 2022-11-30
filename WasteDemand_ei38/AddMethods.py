81a22d98abc2977dcecfdd0a2df5ec029c086bfe
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2022-11-30 19:48:58 +0100

    Submodules for 3.8 & 3.9 - EI3.9 breaks almost everything

---
+++ WasteDemand_ei38/AddMethods.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+AddMethods() : Takes each entry in the custom biosphere database 'waste_db' and creates a new method from it. 
+Eg., ('Waste Footprint', 'waste_dumped_combined', waste_dumped_liquid)
+
+Created on Sat Nov 19 12:21:04 2022
+
+@author: SC-McD
+based of the work of LL
+"""
+
+def AddMethods(project, db_waste):
+
+    import bw2data as bd
+    
+    bd.projects.set_current(project)
+    db_waste = bd.Database(db_waste)
+    dic = db_waste.load()
+                        
+    method_count = len(bd.methods)
+    
+    for key, value in dic.items():
+        unit = value["unit"]
+        code = value["code"]
+        name = value["name"]
+        # name = name.replace("kilogram", "solid")
+        # name = name.replace("cubicmeter", "liquid")
+        
+        ch_factor = 1.0
+        # if unit == "cubic meter":  # to get m^3 into kg (rough, but so are most CFs)
+        #     ch_factor = 1000.0
+            
+        name_combined = "_".join((name.split("_")[0:2])) + "_combined"
+        method_key = ('Waste Footprint', name_combined, name)
+        method_entry = [((db_waste.name, code), ch_factor)]
+        
+        m = bd.Method(method_key)
+        m.register(description=("For estimating the waste demand of an activity: ",name)) 
+        m.write(method_entry)
+        
+        print('* Added: {}\t'.format(str(method_key)))
+
+    methods_added = len(bd.methods) - method_count
+    print("\n*** Added", methods_added, " new methods")
+
+
+def DeleteMethods(project_waste) :
+    
+    import bw2data as bd
+    bd.projects.set_current(project)
+    
+    start = len(bd.methods)
+    print("\n# of methods:", start,"\n")
+    for m in list(bd.methods):
+        if "Waste Footprint" in m:
+            del bd.methods[m]
+            print("deleted:\t", m)
+    
+    finish = len(bd.methods)
+    print("\n# of methods :", finish)
+    print("\n** Deleted {} methods".format(str(start - finish)))
+    
---
+++ WasteSearchVersionComparison.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 19:30:22 2022
+
+@author: stew
+"""
+
+import pandas as pd
+import os
+
+
+path = os.getcwd() + "/WasteSearchResults"
+
+files = [x for x in os.listdir(path) if ".csv" in x and "WasteSearch" not in x]
+files.sort(reverse=True)
+
+
+
+
+col_names = ["ex_code", "ex_name" , "ex_loc" , "ex_amount", "ex_unit" , "db_name", "category"]
+results = pd.DataFrame(columns=col_names)
+
+for file in files:
+    
+    result = pd.read_csv((path +"/"+ file), sep=";", header=None)
+    col_names = ["ex_code", "ex_name" , "ex_loc" , "ex_amount", "ex_unit" , "db_name"]
+    result = result.set_axis(col_names, axis=1)
+    result.insert(0, "category", file.split("_")[1])
+    results = pd.concat([results, result])
+
+
+
+
+
+unique = results.drop_duplicates(subset=['ex_name'], keep='last')
+
+
+results.to_csv((path+ "/WasteSearchCombined.csv"), sep=";", header=True)
+unique.to_csv((path+ "/WasteSearchCombined.csv"), sep=";", header=True)
+
+
+#%% Compare with ei3.8
+
+file ='/home/stew/scripts/gh/WasteDemand/WasteSearchResults/WasteSearchUniqueNames.csv'
+unique38 = pd.read_csv((file), sep=";", index_col=0)
+
+print("Unique names in cutoff38:", len(unique38), ",in cutoff39:", len(unique))
+unique_combined = results = pd.concat([unique, unique38])
+unique_difference = unique_combined.drop_duplicates(subset=['ex_name'], keep='first')
+unique_difference.to_csv((path+ "/WasteSearchUnique38vs39.csv"), sep=";", header=True)
+print(unique_difference.ex_name)
